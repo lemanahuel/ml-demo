9f87d8ab7329139e25c1ec877516050f5eb2a1ee
lemanahuel <lema.nahuel@gmail.com>
2019-05-05 16:46:59 -0300

    IMPROVE, ml demo

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const network = new brain.NeuralNetwork();
-
-network.train([
-  { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { white: 1 } },
-  { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { white: 1 } },
-  { input: { r: 0.33, g: 0.24, b: 0.29 }, output: { dark: 1 } },
-  { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { white: 1 } },
-  { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { dark: 1 } },
-  { input: { r: 1, g: 0.99, b: 0 }, output: { white: 1 } },
-  { input: { r: 1, g: 0.42, b: 0.52 }, output: { dark: 1 } },
-]);
-
-document.querySelector('#color-picker').addEventListener('change', e => {
-  const result = brain.likely(getRgb(e.target.value), network);
-  document.querySelector('body').style.background = e.target.value
-  document.querySelector('#texto-container').style.color = result === 'dark' ? 'white' : 'black'
-});
-
 function getRgb(hex) {
   var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function (m, r, g, b) {
@@ -29,3 +11,49 @@ function getRgb(hex) {
     b: Math.round(parseInt(result[3], 16) / 2.55) / 100,
   } : null;
 }
+
+const network = new brain.NeuralNetwork();
+let defaultData = [
+  { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { white: 1 } },
+  { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { white: 1 } },
+  { input: { r: 0.33, g: 0.24, b: 0.29 }, output: { dark: 1 } },
+  { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { white: 1 } },
+  { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { dark: 1 } },
+  { input: { r: 1, g: 0.99, b: 0 }, output: { white: 1 } },
+  { input: { r: 1, g: 0.42, b: 0.52 }, output: { dark: 1 } }
+];
+
+network.train(defaultData);
+
+function addMoreTrainingData(output) {
+  defaultData.push({ input: currentRgbColor, output })
+  network.train(defaultData);
+  console.log('retrain', currentRgbColor, output);
+}
+
+let $optionA = document.querySelector('#option-a');
+let $optionB = document.querySelector('#option-b');
+let $predicted = document.querySelector('#predicted');
+let currentRgbColor = 0;
+
+document.querySelector('#color-picker').addEventListener('change', e => {
+  currentRgbColor = getRgb(e.target.value);
+  const result = brain.likely(currentRgbColor, network);
+  console.log(result)
+  $optionA.style.background = e.target.value;
+  $optionB.style.background = e.target.value;
+  $predicted.style.background = e.target.value;
+  $predicted.style.color = result === 'dark' ? 'white' : 'black';
+});
+
+$optionA.addEventListener('click', e => {
+  e.preventDefault();
+  addMoreTrainingData({ white: 1 });
+});
+$optionB.addEventListener('click', e => {
+  e.preventDefault();
+  addMoreTrainingData({ dark: 1 });
+});
+$predicted.addEventListener('click', e => {
+  e.preventDefault();
+});
